1113e011b143e00ab929796ddba72a978897cd83
Jianbin Zhang <jianbinzhang@rrc.ca>
2026-01-19 11:04:05 -0600

    new added files.

---
+++ goodhello.py
@@ -0,0 +1,13 @@
+"""This module prints greetings using simple functions."""
+
+def hello():
+    """Prints a simple greeting."""
+    print("Hello, DevOps learner!")
+
+def bye():
+    """Prints a simple farewell."""
+    print("Goodbye!")
+
+if __name__ == "__main__":
+    hello()
+    bye()
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from goodhello import hello, bye
+def test_hello_returns_none():
+    assert hello() is None
+def test_bye_returns_none():
+    assert bye() is None
